d7727d45b9e5052e25519758fef62a48f4bf5d7b
Harman <hamu.dhillon@gmail.com>
2022-08-17 12:06:52 +0530

    17aug

--- pro1/app1/models.py
+++ pro1/app1/models.py
@@ -1,3 +1,10 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class blogPost(models.Model):
+    title=models.CharField(max_length=255)
+    post_by=models.CharField(max_length=255)
+    desc=models.CharField(max_length=1000)
+    image=models.ImageField(upload_to='blog',null=True)
